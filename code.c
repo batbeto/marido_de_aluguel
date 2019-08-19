08c878b78b4738c75648ccb9f14e4f8d64675e4f
brhemme <brhemme7@gmail.com>
2019-08-19 19:20:29 -0300

    update

---
+++ code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct usuario {
+  char nome[81];
+  char password[8];
+  char login[21];
+  char status;
+}
+
+void int_main (void){
+
+
+
+
+
+
+
+    
+}
---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <code.h>
+#include <menus.h>
+
+
+
+int main(void) {
+  int opcao;
+  opcao = imp_menu();
+  while (opcao != 0) {
+    switch (opcao) {
+      case 1 : menu_Contas();
+               break;
+      case 2 : menu_manuCasa();
+               break;
+      case 3 : menu_sobre();
+               break;
+      case 4: menu_Cadastro();
+               break;         
+    }
+    opcao = imp_Menu();
+  }
+   return 0;
+}
