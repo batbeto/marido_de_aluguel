6505666dea8d83ef6011668cfe6037b17bbb12ab
batbeto <btowmaia@gmail.com>
2019-08-27 19:34:07 -0300

    oi

--- main.c
+++ main.c
@@ -4,23 +4,30 @@
  
 
 int main(void) {
-  int opcao;
+  int opcao,opcaologin;
   do{
-    opcao = imp_menu();
-  	switch (opcao) {
-	    case 1 : opcao = menu_Contas();
-               break;
-      case 2 : opcao = menu_manuCasa();
-               break;
-      case 3 : menu_sobre();
-               break;
-      case 4 : opcao = menu_relatorio();
-               break;
-      default :
-            printf("Digite um numero valido");
-  }
+
   
-  }while (opcao != 0);  
+  
+  
+    do{
+      opcao = imp_menu();
+      switch (opcao) {
+        case 1 : opcao = menu_Contas();
+                break;
+        case 2 : opcao = menu_manuCasa();
+                break;
+        case 3 : menu_sobre();
+                break;
+        case 4 : opcao = menu_relatorio();
+                break;
+        default :
+              printf("Digite um numero valido");
+    }
+    
+    }while (opcao != 0);
+  }while(opcaologin != 0);
+      
   
   return 0;
 }
